d042c23de715d9ad14999294b1fb102d6064bf98
DESKTOP-T9MHN3T\sjy <182316962@qq.com>
2019-02-03 14:31:43 +0800

    change

---
+++ github_demo/js/page/HomePage.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { StyleSheet, View,Text } from 'react-native'
+
+class HomePage extends React.Component {
+
+   render() {
+       return (
+           <View>
+               <Text>Hello homepage</Text>
+           </View>
+       )
+   }
+}
+
+const styles = StyleSheet.create({
+   
+})
+
+export default HomePage
---
+++ github_demo/js/page/WelcomePage.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { StyleSheet, View,Text } from 'react-native'
+
+class WelcomePage extends React.Component {
+
+   render() {
+       return (
+           <View>
+               <Text>welcome page</Text>
+           </View>
+       )
+   }
+}
+
+const styles = StyleSheet.create({
+   
+})
+
+export default WelcomePage
